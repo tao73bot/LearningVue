2e49943091be3668b0e70d254608f64ff48e1cb5
Md.Taohidul Islam <mdtaohidul73@student.sust.edu>
2024-10-28 19:05:52 +0600

    Refactor component imports and add RobotBuilder component

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,19 @@
   </header>
   <main>
     <HomePage />
+    <RobotBuilder />
   </main>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue';
+import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
 
 export default {
   name: 'App',
   components: {
     HomePage,
+    RobotBuilder,
   },
 };
 </script>
